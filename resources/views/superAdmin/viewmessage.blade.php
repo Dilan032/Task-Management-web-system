b0a903721dd94d0ab549ce27b27444a3cd9d67b2
Chameesha01 <90745055+Chameesha01@users.noreply.github.com>
2024-09-19 14:28:47 +0530

    Create  a function to add the tasks by super admin side

--- resources/views/superAdmin/viewmessage.blade.php
+++ resources/views/superAdmin/viewmessage.blade.php
@@ -32,6 +32,31 @@
     </style>
 </head>
 <body>
+
+    <!-- Display validation errors -->
+    @if ($errors->any())
+        @foreach ($errors->all() as $error)
+            <script>
+                Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "{{ $error }}",
+                });
+            </script>
+        @endforeach
+    @endif
+
+    @if (session('success'))
+    <script>
+        Swal.fire({
+        icon: "success",
+        title: "{{ session('success') }}",
+        showConfirmButton: false,
+        timer: 1000
+        });
+    </script>
+    @endif
+
     @section('SuperAdminContent')
     <div class="d-flex justify-content-between">
         <div class="justify-content-left">
